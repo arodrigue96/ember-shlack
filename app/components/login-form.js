6218eb8f854d50879a307301926b479a2a836bfd
Aniol Rodriguez <arodrigue96@gmail.com>
2025-05-28 08:53:56 +0200

    Enhance login form to track user selection and update userId display

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,11 +1,27 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
+  @tracked
   userId = null;
+
+  get isDisabled() {
+    return !this.userId;
+  }
 
   loginAsUserWithId(value) {
     console.log('Login as user with ID:', value);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLFormElement}} event
+   */
+
+  @action
+  onSelectChanged(event) {
+    this.userId = event.target.value;
   }
 
   /**
