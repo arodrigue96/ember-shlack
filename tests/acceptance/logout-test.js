4d06865ea3a6a4b89f921a6893ab470bde1fb4ae
Aniol Rodriguez <arodrigue96@gmail.com>
2025-05-29 21:37:05 +0200

    Add MockAuthService for testing logout functionality

--- tests/acceptance/logout-test.js
+++ tests/acceptance/logout-test.js
@@ -1,17 +1,23 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import MockAuthService from '../stubs/auth-service';
 
 module('Acceptance | logout', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /teams and clicking Logout', async function (assert) {
-    await visit('/teams');
+  hooks.beforeEach(function () {
+    this.owner.register('service:auth', MockAuthService);
+  });
 
+  test('visiting /teams and clicking Logout', async function (assert) {
+    await visit('/teams'); //Go to the /teams url
+
+    // await this.pauseTest();
     assert.equal(currentURL(), '/teams', 'Should be on the /teams page');
 
     await click('.team-sidebar__logout-button');
-
+    // await this.pauseTest();
     assert.equal(
       currentURL(),
       '/login',
